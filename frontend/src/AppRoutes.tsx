d3a478ab638904c0b1a903eba8ee606b43992307
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-04-30 19:43:18 -0600

    Update routing in AppRoutes.tsx and add global.css

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "@/components/Home";
+import { Layout } from "@/layouts/Layout";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Home />
+          </Layout>
+        }
+      />
     </Routes>
   );
 };
