7055110fdea6af12b17fc3ee64945bcaff44eacb
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-04-30 19:43:54 -0600

    Update UI components and layouts

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { ReactNode } from "react";
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="flex flex-col min-h-screen">
+      <Header />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
+    </main>
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "@/AppRoutes";
+import "@/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <AppRoutes />
-    </Router>
+      <Router>
+        <AppRoutes />
+      </Router>
   </React.StrictMode>
 );
