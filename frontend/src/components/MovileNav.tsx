7670a0325fbad55592f073359b6ec7c393406cb9
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-04-30 19:43:43 -0600

    Add MainNav and MovileNav components

---
+++ frontend/src/components/MovileNav.tsx
@@ -0,0 +1,31 @@
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+export const MovileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500 " />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          <span className="text-2xl font-bold">
+            Welcome to FoodOrdering.com
+          </span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
