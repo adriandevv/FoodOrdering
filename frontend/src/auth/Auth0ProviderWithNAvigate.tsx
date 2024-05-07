6ee1cd9ba92f89fd799e44463ee1e710fbe55734
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-05-07 17:54:40 -0600

    feat: Add @tanstack/react-query package for API data fetching and mutation

--- frontend/src/auth/Auth0ProviderWithNAvigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNAvigate.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateMyUser } from "@/api/MyUserApi";
 type Props = {
   children: React.ReactNode;
 };
 
 export const Auth0ProviderWithNAvigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
@@ -13,15 +15,16 @@ export const Auth0ProviderWithNAvigate = ({ children }: Props) => {
     throw new Error("Missing required environment variables");
   }
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("user:", user);
-    console.log("appState:", appState);
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirectUri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,27 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "@/AppRoutes";
 import "@/global.css";
 import { Auth0ProviderWithNAvigate } from "./auth/Auth0ProviderWithNAvigate";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNAvigate>
-        <AppRoutes />
-      </Auth0ProviderWithNAvigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNAvigate>
+          <AppRoutes />
+        </Auth0ProviderWithNAvigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
