6081f231107ed3ce4d577089018c904c192c7854
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-05-09 18:48:22 -0600

    chore: Update backend dependencies and add express-oauth2-jwt-bearer package

--- frontend/src/auth/Auth0ProviderWithNAvigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNAvigate.tsx
@@ -10,8 +10,9 @@ export const Auth0ProviderWithNAvigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Missing required environment variables");
   }
   const onRedirectCallback = () => {
@@ -22,7 +23,7 @@ export const Auth0ProviderWithNAvigate = ({ children }: Props) => {
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri, audience }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
