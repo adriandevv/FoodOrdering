79f070600b5f8d45abc82800bb9807231e4b78c8
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-05-25 17:03:31 -0600

    chore: Update backend to use consistent import formatting and add health check endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 
 mongoose.connect(process.env.MONGOBD_CONNECTION_STRING as string).then(() => {
@@ -11,8 +11,11 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.get("/health", (req: Request, res: Response) => {
+  res.send({ message: "Server is running!" });
+});
 
-app.use("/api/my/user",myUserRoute);
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
   console.log("Server is running on port 7000");
