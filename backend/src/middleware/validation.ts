f684527612e34859905419dce3b943c4ca4ce29f
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-05-30 17:31:20 -0600

    feat: Add API endpoint for creating a new restaurant

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -6,12 +6,11 @@ const handleValidationErrors = (
   res: Response,
   next: NextFunction
 ) => {
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
 };
 
 export const validateMyUserRequest = [
@@ -22,5 +21,36 @@ export const validateMyUserRequest = [
     .withMessage("Address Line 1 must be a string"),
   body("city").isString().notEmpty().withMessage("City must be a string"),
   body("country").isString().notEmpty().withMessage("Country must be a string"),
-  handleValidationErrors
+  handleValidationErrors,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName")
+    .isString()
+    .notEmpty()
+    .withMessage("Restaurant Name is required"),
+  body("city").isString().notEmpty().withMessage("City must is required"),
+  body("country").isString().notEmpty().withMessage("Country is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .notEmpty()
+    .withMessage("Delivery Price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .notEmpty()
+    .withMessage("Estimated Delivery Time must be a positive number"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .notEmpty()
+    .withMessage("Cuisines must not be empty"),
+  body("menuItems").isArray().withMessage("Menu Items must be an array"),
+
+  body("menuItems.*.name").notEmpty().withMessage("Menu Item Name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu Item Price must be a positive number"),
+
+  handleValidationErrors,
 ];
